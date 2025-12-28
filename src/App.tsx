972920b544a6b2ebe537a91d5e667e00c78e174d
Seongnam Hong <hongdev26@gmail.com>
2025-12-29 00:33:38 +0900

    Feat : supabase 데이터 연동 테스트 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
+import { useEffect } from "react";
+import supabase from "./lib/supabase";
+
 function App() {
-  return (
-    <>
-    </>
-  );
+  const loginToGithub = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: "http://localhost:5173/auth/callback",
+      },
+    });
+  };
+
+  useEffect(() => {
+    const handleSession = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      if (!session) return;
+
+      const { error } = await supabase.from("users").upsert(
+        {
+          id: session.user.id,
+          github_username: session.user.user_metadata.preferred_username,
+        },
+        { onConflict: "id" }
+      );
+
+      if (error) {
+        console.error("User upsert error:", error);
+      } else {
+        console.log("users upsert 성공");
+      }
+    };
+
+    handleSession();
+  }, []);
+
+  return <button onClick={loginToGithub}>GitHub 로그인</button>;
 }
 
 export default App;
